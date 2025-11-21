14d3a41f1c984fccf1d3281100e7b2e0d66a6c0b
Võ Nhạc Phước <vonhacphuoc@gmail.com>
2025-11-21 12:17:41 +0700

    add model OrderDetail

---
+++ Models/OrderDetail.cs
@@ -0,0 +1,10 @@
+namespace Backend_Cuoiky.Models
+{
+    public class OrderDetail
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
