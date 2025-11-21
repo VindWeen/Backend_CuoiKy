56aa89234ffad8c0acbfe91411aa0899ec2189b9
VindWeen <fw62262@gmail.com>
2025-11-21 23:03:50 +0700

    Login/Register, Product, ProductManager(chưa có nhận token admin)

--- obj/Debug/net8.0/Backend_CuoiKy.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend_CuoiKy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27db6adc6ca6c15756727619a40b258eeac05a28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a6a12b990732a587e7c09a6574d98ddaec7133b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
