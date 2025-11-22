1de9bf638218be28b72fe6225aff160dd6cb34e8
Võ Nhạc Phước <vonhacphuoc@gmail.com>
2025-11-22 12:08:25 +0700

    merge VNP

--- obj/Debug/net8.0/Backend_CuoiKy.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend_CuoiKy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f8dc274b0a938c3abe6ab68da35d5939940c74c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93213378cb543eec7cfa2311e62890eb73cca4b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
