62d7ee3c2fa087c9419a12126bd1a2de56474a24
Võ Nhạc Phước <vonhacphuoc@gmail.com>
2025-11-22 12:44:03 +0700

    merge TTVQ

--- obj/Debug/net8.0/Backend_CuoiKy.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend_CuoiKy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93213378cb543eec7cfa2311e62890eb73cca4b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cc37053246ae15ecf77be9a62471d29d208cdba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
