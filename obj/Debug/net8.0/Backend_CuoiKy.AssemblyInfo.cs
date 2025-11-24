c26ea2037e6d784b62b9ec995f47dfba92997c42
Tạ Trần Vinh Quang <fw62262@gmail.com>
2025-11-24 22:54:03 +0700

    sửa vị trí nút đăng xuất trên sidebar của customer

--- obj/Debug/net8.0/Backend_CuoiKy.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend_CuoiKy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8208a00b662cd6cb278b6872388dbb5d299700e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7b8b3dd1914404c81bd647333850ad27f75b59e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_CuoiKy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
